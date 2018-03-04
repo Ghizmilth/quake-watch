b13b9c3f8cb2cd86cbcb36f33658c255d92ff9f1
Ghizmilth <hidanerferrer@gmail.com>
2018-03-04 11:56:16 -0800

    added the secure link to quakes gov https

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,6 @@
 // globals
 var weekly_quakes_endpoint =
-  "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojson";
+  "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojson";
 var $info_row_target;
 var map;
 
